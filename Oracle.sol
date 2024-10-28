d57d281057254504202db41bc145aede87df24c6
Lucio <11446999+lucio0256@users.noreply.github.com>
2024-10-28 22:38:25 +0100

    Update Oracle.sol

--- Oracle.sol
+++ Oracle.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Oracle {
+contract Oracle is Ownable {
     mapping (address => bool) pendingRequests;
     event NewRequest(address from);
+
+    constructor() Ownable(msg.sender) {}
 
     function createRequest() external {
         require(!pendingRequests[msg.sender], "Your request already pending");
@@ -11,7 +14,7 @@ contract Oracle {
         emit NewRequest(msg.sender);
     }
 
-    function setEthPrice(address _consumer, uint _price) external {
+    function setEthPrice(address _consumer, uint _price) external onlyOwner {
         require(pendingRequests[_consumer], "You are not in requests list");
         (bool success, ) = _consumer.call(abi.encodeWithSignature("_callback(uint256)", _price));
         require(success, "Error setting ethPrice");
